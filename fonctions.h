db510c6608ec7d5bc7e9526014675d98ff677389
stain <designab33@gmail.com>
2022-11-02 12:34:25 +0100

    structures gestion bureau de vote

---
+++ fonctions.h
@@ -0,0 +1,16 @@
+#ifndef FONCTIONS_H_
+#define FONCTIONS_H_
+}
+typedef struct agents 
+{char prenom[30];
+char nom [30]; 
+double cin;
+}agents;
+typedef struct bureau_vote { 
+char etat[100]; 
+char municipalite[100] ;
+float capacite; 
+agents agent; 
+} 
+bureau_vote; 
+#endif 
