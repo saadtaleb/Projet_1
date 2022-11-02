f658e00395c61e23be486fca251fdcd03867e1e2
jdidi balsen <115507925+balsen@users.noreply.github.com>
2022-11-02 19:16:49 +0100

    Add files via upload

---
+++ struct.h
@@ -0,0 +1,10 @@
+#ifndef STRUCT_H_INCLUDED
+#define STRUCT_H_INCLUDED
+type def struct observateur{
+char id [30];
+char nom [30];
+char prenom[30];
+int num_bureau;}observateur;
+
+
+#endif // STRUCT_H_INCLUDED
