08f4e56599bc92e58f83bce605214ccc46589e81
stastix <designab33@gmail.com>
2022-11-02 15:36:09 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -7,10 +7,14 @@ char nom [30];
 double cin;
 }agents;
 typedef struct bureau_vote { 
+int numer_bureau; 
 char etat[100]; 
 char municipalite[100] ;
-float capacite; 
-agents agent; 
+char adress[100]; 
+int capacite_e; 
+int capacite_o;
+agents agent;
+ 
 } 
 bureau_vote; 
 #endif 
