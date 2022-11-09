cb55db348ce8a131c684daec40de1a6b6695f118
jdidi balsen <115507925+balsen@users.noreply.github.com>
2022-11-09 17:17:51 +0100

    Update struct.h

--- struct.h
+++ struct.h
@@ -4,7 +4,9 @@ type def struct observateur{
 int id;
 char nom [30];
 char prenom[30];
-int num_bureau;}observateur;
+int num_bureau;
+char nationalite[30];
+char profession[30];}observateur;
 
 
 #endif // STRUCT_H_INCLUDED
